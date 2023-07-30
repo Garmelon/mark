b29f22a5a8ec89abe12236e7eed8c2f9f62ead22
Joscha <joscha@plugh.de>
2023-07-30 15:54:35 +0200

    Parse cli args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::path::PathBuf;
+
+use clap::Parser;
+
+#[derive(Debug, clap::Parser)]
+struct BwCmd {}
+
+#[derive(Debug, clap::Parser)]
+enum Cmd {
+    Bw(BwCmd),
+}
+
+#[derive(Debug, clap::Parser)]
+struct Args {
+    #[arg(long, short)]
+    r#in: Option<PathBuf>,
+    #[arg(long, short)]
+    out: Option<PathBuf>,
+    #[command(subcommand)]
+    cmd: Cmd,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    println!("{args:#?}");
 }
